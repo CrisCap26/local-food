722bbc3d072a565e05d7b6b0e587e1e037ce521b
crissdot <crissdotdev@gmail.com>
2022-11-03 19:21:59 -0600

    fix use local storage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,22 +3,17 @@ import React from 'react';
 const useLocalStorage = (key) => {
   const [item, setItem] = React.useState();
 
+  const getItem = () => {
+    return item;
+  }
+
   const saveItem = (value) => {
-    const localStorageItem = localStorage.getItem(key);
-    let parsedItem;
-
-    if (!localStorageItem) {
-      localStorage.setItem(key, JSON.stringify(value));
-      parsedItem = value;
-    } else {
-      parsedItem = JSON.parse(localStorageItem);
-    }
-
-    setItem(parsedItem);
+    localStorage.setItem(key, JSON.stringify(value));
+    setItem(value);
   }
 
   return {
-    item,
+    getItem,
     saveItem,
   };
 }
