acc235c4cfb0939e3da4865b3d598285862990cf
crissdot <crissdotdev@gmail.com>
2023-01-12 18:39:55 -0600

    add submit profile image when create a user

--- src/pages/RegistrarUsuario.jsx
+++ src/pages/RegistrarUsuario.jsx
@@ -1,5 +1,6 @@
 import "./reg_usu.css";
 import { useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 import logo from "../imgs/logo.png";
 import { useState, useEffect } from "react";
 import {expresiones} from './utils'
@@ -8,124 +9,41 @@ import mal from '../imgs/cerrar.png'
 import { create } from '../services/userService';
 
 function RegistrarUsuario() {
-
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
-  const [user, setUser] = useState("");
-  const [nombre, setNombre] = useState("");
-  const [apellidos, setApellidos] = useState("");
-  const [telefono, setTelefono] = useState("");
   const [isChecked, setIsChecked] = useState(false);
   const [validPasswords, setValidPasswords] = useState(false);
+
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+    const formData = new FormData();
+    formData.append('username', data.username);
+    formData.append('password', data.password);
+    formData.append('name', data.username);
+    formData.append('last_name', data.last_name);
+    formData.append('phone_number', data.phone_number);
+    formData.append('email', data.email);
+    formData.append('profile_image', data.profile_image[0]);
 
-  useEffect(() => {
-    validarPass2();
-  }, [password, password2]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const userToRegister = {
-      password: password,
-      username: user,
-      name: nombre,
-      last_name: apellidos,
-      phone_number: telefono,
-      email: email,
-    };
-
-    create(userToRegister).then(data => {
-      console.log('User created succesfully', data);
+    create(formData).then(res => {
+      console.log('User created succesfully', res);
       navigate('/login');
     });
-
   };
 
-  const validarEmail = () => {
-    if(email.length > 0) {
-      if(!expresiones.correo.test(email)) {
-        console.log('El email es invalido')
-        // document.getElementById("invalidEmail").style.display = "block";
-        // document.getElementById("btn-enviar").disabled = true;
-      } else {
-        console.log('El email es valido')
-        // document.getElementById("invalidEmail").style.display = "none";
-        // document.getElementById("btn-enviar").disabled = false;
-      }
-    }
-  }
-
-  const validarPass = () => {
-    console.log(password)
-    console.log(password2)
-    if(password.length >= 6 && password2.length >= 6) {
-      console.log("contraseña correcta")
-      // document.getElementById("invalidPass").style.display = "none";
-      return true
-    } else {
-      console.log("la contraseña debe tener de 6 a 12 carateres")
-      // document.getElementById("invalidPass").style.display = "block";
-      return false
-    }
-  }
-
-  const validarPass2 = () => {
-    if(validarPass() === true) {
-      if(password !== password2) {
-        setValidPasswords(false);
-        // console.log(' las contraseñas no son iguales ')
-        // document.getElementById("message1").style.display = "block";
-        // document.getElementById("message2").style.display = "none";
-        // document.getElementById("btn-enviar").disabled = true;
-      } else {
-        console.log('Las contraseñas son iguales')
-        setValidPasswords(true);
-        // document.getElementById("message2").style.display = "block";
-        // document.getElementById("message1").style.display = "none";
-        // document.getElementById("btn-enviar").disabled = false;
-      }
-    } else {
-      setValidPasswords(false);
-      // document.getElementById("message1").style.display = "none";
-      // document.getElementById("message2").style.display = "none";
-    }
-  }
-
-  /*function createUser() {
-    const usuario = {
-      "password": password,
-      "username": user,
-      "name": nombre,
-      "last_name": apellidos,
-      "phone_number": telefono,
-      "email": email,
-    }
-    console.log(usuario)
-
-    axios.post('http://127.0.0.1:8000/usuarios/', {
-      "password": password,
-      "username": user,
-      "name": nombre,
-      "last_name": apellidos,
-      "phone_number": telefono,
-      "email": email,
-    })
-    .then(function (response){
-      console.log(response);
-      console.log(response.data);
-    })
-  }*/
+  useEffect(() => {
+    setValidPasswords(password === password2);
+    console.log(password, password2)
+  }, [password, password2]);
 
   const onCheckedChange = () => {
     const toggle = !isChecked;
     setIsChecked(toggle);
   }
 
-
   return (
-    <form className="user_form" onSubmit={handleSubmit} method="post">
+    <form className="user_form" onSubmit={handleSubmit(onSubmit)} >
       <center>
         <h1 className="user_form_titulo">Formulario de Registro</h1>
       </center>
@@ -143,54 +61,46 @@ function RegistrarUsuario() {
         <div className="field">
           <label>Usuario</label>
           <input
-            type="text"
-            name="user"
             id="usuario"
             placeholder="Nombre de Usuario"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
+            {...register('username', {
+              required: true,
+            })}
           />
         </div>
         <div className="field">
           <label>Nombre</label>
           <input
-            type="text"
-            name="nombre"
             id="nombre"
-            onChange={(e) => setNombre(e.target.value)}
-            value={nombre}
+            {...register('name')}
           />
         </div>
         <div className="field">
           <label>Apellidos</label>
           <input
-            type="text"
-            name="apellidos"
             id="apellidos"
-            onChange={(e) => setApellidos(e.target.value)}
-            value={apellidos}
+            {...register('last_name')}
             />
         </div>
         <div className="field">
           <label>Telefono</label>
           <input
-            type="text"
-            name="telefono"
             id="tel"
-            onChange={(e) => setTelefono(e.target.value)}
-            value={telefono}
+            {...register('phone_number')}
             />
         </div>
         <div className="field">
           <label>E-mail</label>
           <input
-            type="email"
-            name="email"
             id="correo"
-            onChange={(e) => setEmail(e.target.value)}
-            onBlur={validarEmail}
-            value={email}
+            {...register('email', {
+              validate: (email) => {
+                if (email.length === 0) {
+                  return true;
+                }
+                return expresiones.correo.test(email);
+              },
+            })}
           />
         </div>
         <p id="invalidEmail" className="noCoinciden">El email es invalido</p>
@@ -198,11 +108,14 @@ function RegistrarUsuario() {
           <label>Contraseña</label>
           <input
             type="password"
-            name="password"
             id="contra"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
+            {...register('password', {
+              required: true,
+              minLength: 6,
+              onChange: (e) => {
+                setPassword(e.target.value);
+              },
+            })}
           />
         </div>
         {/* <p id="invalidPass" className="noCoinciden">La contraseña debe tener entre 6 y 12 caracteres</p> */}
@@ -210,26 +123,29 @@ function RegistrarUsuario() {
           <label>Repetir contraseña</label>
           <input
             type="password"
-            name="Contraseña2"
             id="contra2"
-            onChange={(e) => {
-              setPassword2(e.target.value)
-            }
-          }
-            value={password2}
-            required
+            {...register('password2', {
+              required: true,
+              minLength: 6,
+              onChange: (e) => {
+                setPassword2(e.target.value);
+              },
+            })}
           />
         </div>
-
           {!validPasswords &&
             <p id="message1" className="pass_noCoinciden">Las contraseñas no coinciden
               <img className="icon" src={mal}/>
             </p>
           }
-
-        <p id="message2" className="coinciden">Las contraseñas coinciden
-        <img className="icon" src={cheque} />
-        </p>
+        <div className="field">
+          <label>Imagén de perfil</label>
+          <input
+            type="file"
+            id="profileImg"
+            {...register('profile_image')}
+          />
+        </div>
 
         <div className="privacidad">
           <input
@@ -242,7 +158,7 @@ function RegistrarUsuario() {
         </div>
         <div className="submit">
           <center>
-            <button id="btn-enviar" disabled={!isChecked || !validPasswords || user.length === 0}>Registrarme</button>
+            <button id="btn-enviar" type="submit" disabled={!isChecked || !validPasswords}>Registrarme</button>
           </center>
         </div>
       </div>
