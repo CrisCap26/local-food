7e87ec16863ce20a920cb2ec090b18b0b578d670
crissdot <crissdotdev@gmail.com>
2023-01-12 18:53:00 -0600

    show current img profile when get my profile

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -4,15 +4,23 @@ import { useParams } from 'react-router-dom';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { get } from '../../services/userService';
 import imgUser from '../../imgs/icon-user.png';
+import { config } from '../../config';
 
 const User = () => {
   const params = useParams();
   const { getItem: getToken } = useLocalStorage('token');
   const [user, setUser] = React.useState(null);
+  const [profileImage, setProfileImage] = React.useState(imgUser);
 
   React.useEffect(() => {
     get(getToken(), params.userId).then(response => {
-      setUser(response.data);
+      const fetchedUser = response.data;
+      setUser(fetchedUser);
+      if (fetchedUser.profile_image) {
+        setProfileImage(config.backendUrl + fetchedUser.profile_image);
+      } else {
+        setProfileImage(imgUser);
+      }
     });
   }, []);
 
@@ -20,7 +28,7 @@ const User = () => {
     <main className="user">
       <h1>Hola {user?.username}</h1>
       <div className='container'>
-      <img src={imgUser}></img>
+      <img src={profileImage}></img>
       <div className='container-info-user'>
         <div className='info-user'>Nombre: <p>{user?.name}</p></div>
         <div className='info-user'>Apellidos: <p>{user?.last_name}</p></div>
