12c1bdec598a086fc125ed63b6b211a5d8a87c9c
Cris <cristopher.26hp@gmail.com>
2023-05-06 00:43:50 -0600

    Agregando elementos al home en vista desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import bandeja from "../imgs/bandeja.png";
+import plato from "../imgs/comer.png"
+import tienda from "../imgs/tienda.png"
+import personas from "../imgs/personas.png"
+import cel from "../imgs/compartirComida.png"
 import './index.css'
 import Searcher from "../components/Searcher";
 
@@ -23,28 +27,46 @@ function Home() {
           </h2>
         </section>
         <section className="inicio_info">
-          <h2 className="inicio_info__title">¡Nos preocupamos por ti!</h2>
-          <center>
+          <div className="nos-preocupamos">
+            <h2 className="inicio_info__title">¡Nos preocupamos por ti!</h2>
+            <center>
+              <img
+                src={bandeja}
+                style={{ align: "center", width: "80", height: "70" }}
+                alt="Unos tacos"
+                className="inicio_info__image"
+              />
+            </center>
+          </div>
+          <div className="div-info">
+            <p>
+              Te mostramos los negocios de tu localidad para que no te
+              pierdas de los deliciosos platillos que ofrecen.
+            </p>
             <img
-              src={bandeja}
-              style={{ align: "center", width: "80", height: "70" }}
+              src={plato}
+
               alt="Unos tacos"
               className="inicio_info__image"
             />
-          </center>
-
-          <center>
-            {/* <a href="#" className="inicio_info__button">
-              Mirar los platillos
-            </a> */}
-          </center>
+          </div>
         </section>
 
         <section className="inicio_nego">
-          <p className="inicio_text">
-            ¡Apoya a estos increíbles negocios donde podrás degustar una gran
-            variedad de platillos hasta la puerta de tu casa!
-          </p>
+          <div className="cont-nego">
+            <p className="inicio_text">
+              ¡Apoya a estos increíbles negocios donde podrás degustar una gran
+              variedad de platillos hasta la puerta de tu casa!
+            </p>
+            <img
+              src={tienda}
+              align="center"
+              className="inicio_res__info"
+              width="190"
+              height="90"
+            />
+          </div>
+
           <br />
           <div className="container_negocios">
             <img
@@ -61,29 +83,37 @@ function Home() {
         </section>
 
         <section className="inicio_regis">
-          <center className="inicio_regis__title">
-            {/*<font size={6} face="impact" color="#f82510">
-              Únete a LocalFood
-          </font>*/}
-            <h3>Únete a LocalFood</h3>
-          </center>
-          <br />
-          <div className="inicio_regis__container">
-          <center>
+          <div className="cont-unir">
+            <center className="inicio_regis__title">
+              <h3>Únete a LocalFood</h3>
+            </center>
             <img
-              src="./unete2.jpg"
+              src={personas}
               className="inicio_info__reg"
               width={200}
               height={90}
               align="middle"
             />
-          </center>
+            <p>Conoce nuestra maravillosa comunidad</p>
+          </div>
+
           <br />
-          <center>
-            <a href="/AcercaDeNosotros" className="inicio_reg__boton">
-              Conocer más
-            </a>
-          </center>
+          <div className="inicio_regis__container">
+            <center>
+              <img
+                src="./unete2.jpg"
+                className="inicio_info__reg"
+                width={200}
+                height={90}
+                align="middle"
+              />
+            </center>
+            <br />
+            <center>
+              <a href="/AcercaDeNosotros" className="inicio_reg__boton">
+                Conocer más
+              </a>
+            </center>
           </div>
         </section>
 
@@ -101,19 +131,26 @@ function Home() {
           </center>
           <br />
           <div className="inicio_restaurant_container">
-          <p className="inicio_texto">
-            Presume tus platillos para que los usuarios prueben el verdadero
-            sabor de la comida mexicana.
-          </p>
-          <br />
-          <center>
-            <a
-              href="./RegistrarRestaurante"
-              className="inicio_resta__button"
-            >
-              Registrar Negocio
-            </a>
-          </center>
+          <img
+              src={cel}
+              className="inicio_reg__res"
+              width={200}
+              height={90}
+              align="middle"
+            />
+            <p className="inicio_texto">
+              Presume tus platillos para que los usuarios prueben el verdadero
+              sabor de la comida mexicana.
+            </p>
+            <br />
+            <center>
+              <a
+                href="./RegistrarRestaurante"
+                className="inicio_resta__button"
+              >
+                Registrar Negocio
+              </a>
+            </center>
           </div>
         </section>
       </main>
